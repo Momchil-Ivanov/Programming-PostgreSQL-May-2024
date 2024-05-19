1de3eac6ad899359b2f566ec0e3b9410bb490734
Momchil Ivanov <88820552+Momchil-Ivanov@users.noreply.github.com>
2024-05-19 17:12:30 +0300

    Add files via upload

---
+++ 6.basic_crud_exercise/20.create_a_view_with_multiple_tables.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE VIEW
+	view_addresses
+AS
+SELECT
+	CONCAT_WS(
+	' ',
+	e.first_name,
+	e.last_name
+	) AS full_name,
+	department_id,
+	CONCAT_WS(
+	' ',
+	a.number,
+	a.street
+	) AS address
+FROM
+	employees AS e, addresses as a
+WHERE
+	a.id = e.address_id
+ORDER BY
+	address ASC;
