0e54fd14da001a9097f314d3818507e7eb5c7c6b
Momchil Ivanov <88820552+Momchil-Ivanov@users.noreply.github.com>
2024-05-28 23:27:59 +0300

    Add files via upload

---
+++ 11.table_relations_lab/01.mountains_and_peaks.sql
@@ -0,0 +1,15 @@
+CREATE TABLE mountains(
+	id serial PRIMARY KEY,
+	name VARCHAR(50)
+);
+
+CREATE TABLE peaks(
+	id serial PRIMARY KEY,
+	name VARCHAR(50),
+	mountain_id INT
+);
+
+ALTER TABLE peaks
+ADD CONSTRAINT fk_peaks_mountains
+	FOREIGN KEY (mountain_id)
+	REFERENCES mountains(id)
